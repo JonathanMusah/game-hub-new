7c3d2bee7be51dc529957bf0e1af50ccaebecfd4
Jonathan Musah <109772544+JonathanMusah@users.noreply.github.com>
2023-07-13 20:20:53 +0000

    show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,16 @@
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImageUrl from '../services/image-url'
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
-interface Props{
-    game: Game
+interface Props {
+  game: Game;
 }
 
-function GameCard({game}: Props) {
+function GameCard({ game }: Props) {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card width={"300px"} borderRadius={10} overflow={"hidden"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
@@ -25,4 +25,4 @@ function GameCard({game}: Props) {
   );
 }
 
-export default GameCard
+export default GameCard;
